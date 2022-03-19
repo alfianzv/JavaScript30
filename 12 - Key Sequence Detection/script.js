14d0a41861d813d1b42c21f547dd5ee2f51957d4
alfianzv <alfian101@gmail.com>
2022-03-19 17:31:31 +0700

    day 12: definitely something i could use if i chose to make a personal website

---
+++ 12 - Key Sequence Detection/script.js
@@ -0,0 +1,13 @@
+const pressed = [];
+const secretcode = 'wesbos';
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretcode.length - 1, pressed.length - secretcode.length);
+    if (pressed.join('').includes(secretcode)) {
+        console.log('DING DING');
+        cornify_add();
+    };
+    console.log(pressed);
+})
